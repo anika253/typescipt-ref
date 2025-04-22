820091fa5989d4979e9985d4513a39cf572170f2
anikas <22bec020@nith.ac.in>
2025-04-23 01:58:28 +0530

    1

---
+++ index.ts
@@ -0,0 +1,11 @@
+let x = 1; //type interferencing ..is is plain js
+console.log(x);
+
+let y: number = 1; // it is typescipt code
+console.log(y); // specify its type
+
+//index.js m only js only here
+
+/*let z: number = 1;
+z = "harkirat";
+console.log(z);*/
